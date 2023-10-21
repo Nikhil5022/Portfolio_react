7d81872f7c58ffce0fd2fe601170bd453f69e782
nikhil5022 <gsn.agc5022@gmail.com>
2023-10-21 21:52:03 +0530

    typed

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import './About.css';
 import bgImage from './Images/bg.png';
+import Typed from 'typed.js';
 
 
 export default function Home() {
+    const el = React.useRef(null);
+
+    React.useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ['<i>Full Stack Developer.</i>', '<i>Web Developer.</i>', '<i>Software Engineer.</i>'],
+            typeSpeed: 50,
+            backSpeed: 50,
+            loop: true,
+        });
+
+        return () => {
+            // Destroy Typed instance during cleanup to stop animation
+            typed.destroy();
+        };
+    }, []);
     return (
         <>
             <div className='animate'>
@@ -16,6 +32,9 @@ export default function Home() {
                             I am a passionate{' '}
                         </h2>{' '}
                         <br />
+                        <h2 style={{ color: '#aa6be4' }}>
+                            <span ref={el} />
+                        </h2>
                     </div>
                     <div className="picture">
                         <img src={bgImage} alt="" />
