7741b243737fe48cb81d81c7bcbdc00be70e5cc7
nikhil5022 <gsn.agc5022@gmail.com>
2023-10-21 20:44:38 +0530

    added home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import ContactUs from './Contactus.js'
+import Home from './Home.js'
 
 function App() {
   return (
     <div className="App">
+      <Home />
       <ContactUs />
     </div>
   );
